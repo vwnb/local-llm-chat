6e5fe73a1ca4028834dbc6d06bc3af5d1373c677
Ville Kemppainen <villekemppainen@Villes-MacBook-Pro.local>
2025-05-07 14:28:48 +0300

    Add divider and logo

---
+++ src/app/components/Divider.tsx
@@ -0,0 +1,5 @@
+export default function Divider() {
+  return (
+    <div className="border-t border-gray-300 my-6" />
+  )
+}
---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Logo() {
+  return (
+    <div className="flex items-center justify-center gap-8">
+      <div className="logo text-large">
+        🎐
+      </div>
+      <h1 className="text-xl">風鈴 AI</h1>
+    </div>
+  )
+}
