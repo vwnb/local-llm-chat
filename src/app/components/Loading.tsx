83f29193e0e053c1ef7a1149ea499fd1b291ee83
Ville Kemppainen <villekemppainen@Villes-MacBook-Pro.local>
2025-05-08 14:36:40 +0300

    Styling

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,8 +2,8 @@
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex w-8 h-8 text-4xl rounded-full animate-spin text-center justify-center items-center">🌸</div>
+    <div className="flex p-4 items-center justify-center">
+      <div className="flex w-8 h-8 text-3xl rounded-full animate-spin text-center justify-center items-center">🌸</div>
     </div>
   )
 }
